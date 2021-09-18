d41ad8b8c157920887194d7ed315448c71698c80
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-09-19 00:07:05 +0300

    form activation

--- javas/java.js
+++ javas/java.js
@@ -57,4 +57,14 @@ $(document).ready(function () {
         }
 
     });
+    /*form info*/
+    $('form#contactForm').submit(function (event) {
+        event.preventDefault();
+        var name = $('#name').val();
+        var pass = $('#email').val();
+        var mess = $('#mess').val();
+        alert("Hi " + name + " we have received your message and we will get in touch. Thank you for contacting us.");
+    });
 
+
+});
