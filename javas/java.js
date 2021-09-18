550d1f9c27664803e4248ffd3c9593511406ef52
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-09-18 23:43:18 +0300

    declare functions

--- javas/java.js
+++ javas/java.js
@@ -6,3 +6,16 @@ function Order(type, size, crust, topping) {
     this.crust = crust;
     this.topping = topping;
 }
+Order.prototype.fullOrder = function () {
+    return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
+};
+
+function Total(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+}
+
+Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+};
