482a0a8a667da94df65ef1829f53c1ec198baf6f
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-09-18 23:46:34 +0300

    declares variables

--- javas/java.js
+++ javas/java.js
@@ -19,3 +19,42 @@ function Total(price, quantity, delivery) {
 Total.prototype.finalTotal = function () {
     return this.price * this.quantity + this.delivery;
 };
+var sizePrice = [1200, 900, 600]
+var deliverPrices = [0, 200];
+//user interface logic
+$(document).ready(function () {
+    $('form#myForm').submit(function (event) {
+        event.preventDefault();
+        var pizzaType = $('#type').val();
+
+        var pizzaSize = parseInt($('#size').val());
+
+        var pizzaCrust = $('#crust').val();
+
+        var pizzaTop = $('#top').val();
+
+        var pizzaQty = parseInt($('#qty').val());
+
+        var pizzaPick = parseInt($('#pick').val());
+
+        var price = sizePrice[pizzaSize - 1];
+
+
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
+
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
+        alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+        alert("your bill is: " + newTotal.finalTotal());
+        }else{
+            if(pizzaPick===2){
+                prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal());
+            }
+        }
+
+    });
+
